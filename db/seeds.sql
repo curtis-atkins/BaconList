d5f1f8bb61fd1114d7825dbb52c0b5053118c319
jeremyhe <jeremyhe1@gmail.com>
2017-08-31 19:13:01 -0400

    bacon1

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO itemList (item_name, sold) VALUES ('Item1', FALSE);
+INSERT INTO itemList (item_name, sold) VALUES ('Item2', FALSE);
+INSERT INTO itemList (item_name, sold) VALUES ('Item3', FALSE);
---
+++ models/bacon.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm");
+
+var bacon = {
+  all: function(cb) {
+    orm.all("itemList", function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create("itemList", ["item_name", "sold"], [name, false], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("itemList", {
+      devoured: true
+    }, condition, cb);
+  }
+};
+
+module.exports = bacon;
