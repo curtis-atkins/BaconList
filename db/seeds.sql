67faa5963cabd6bd8427c5d674611a88b105c4d9
jeremyhe <jeremyhe1@gmail.com>
2017-09-06 20:26:40 -0400

    stuff

--- db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO itemList (item_name, sold) VALUES ('Item1', FALSE);
-INSERT INTO itemList (item_name, sold) VALUES ('Item2', FALSE);
-INSERT INTO itemList (item_name, sold) VALUES ('Item3', FALSE);
