1a1195005c138265dc9d25ff5e47db378e150a91
catkins <catkinsjr28@gmail.com>
2017-09-09 10:35:06 -0400

    facebook authentication working

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,7 @@
 // Here is the O.R.M. where you write functions that takes inputs and conditions
 // and turns them into database commands like SQL.
 
-var connection = require("./connection");
+var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
   var arr = [];
