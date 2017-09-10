2a32346fd1428445d4c9dafba6535618073d0226
catkins <catkinsjr28@gmail.com>
2017-09-10 18:07:13 -0400

    add empty PW string for connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Hypeman28!!",
+    password: "",
     database: "bacon_db"
 });
 
