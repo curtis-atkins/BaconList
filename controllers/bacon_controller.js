e06dd538ad04dd06f36ce51c283019a55dc29174
catkins <catkinsjr28@gmail.com>
2017-09-13 19:23:30 -0400

    added local storage

--- controllers/bacon_controller.js
+++ controllers/bacon_controller.js
@@ -13,7 +13,7 @@ router.get("/login", function(req, res) {
 });
 
 router.get("/", function(req, res) {
-  res.redirect("/login");
+  res.redirect("/bacon");
 });
 
 router.get("/bacon", function(req, res) {
