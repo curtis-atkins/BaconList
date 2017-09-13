a38a07b530bd67faff3643936a6d00f53a28a922
jeremyhe <jeremyhe1@gmail.com>
2017-09-12 20:54:39 -0400

    new models

---
+++ models/transactionList.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm");
+
+var transactionList = {
+  all: function(cb) {
+    orm.all("itemList", function(res) {
+      cb(res);
+    });
+  },
+  create: function(table, item_info, cb) {
+    var col = [];
+    var val = [];
+    for (let key in item_info) {
+      col.push(key);
+      val.push(item_info[key]);
+    }
+    orm.create(table, col, val, cb);
+  },
+};
+
+module.exports = transactionList;
