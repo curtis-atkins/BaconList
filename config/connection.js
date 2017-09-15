0384210da7593ecdfd79e3a2bd388707fb040a4c
catkins <catkinsjr28@gmail.com>
2017-09-14 21:53:14 -0400

    FB login/ logout works

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,8 @@ var connection = mysql.createConnection({
 });
 
 
-/*
-var connection = mysql.createConnection({
+
+/*var connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "root",
